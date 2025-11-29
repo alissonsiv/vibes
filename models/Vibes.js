1f082208e28da4148d674adbbdcb77c97aa5c45c
Alisson <alissonmoura890@gmail.com>
2025-11-28 23:53:58 -0300

    Definição dos models e atributos iniciais

---
+++ models/Vibes.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize'
+import db from '../db/conn.js'
+
+import User from './User.js'
+
+// Define o model Vibe
+const Vibe = db.define('Vibe', {
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+// Define relacionamento: cada Vibe pertence a um User
+Vibe.belongsTo(User)
+
+// Define relacionamento: cada User pode ter várias Vibes
+User.hasMany(Vibe)
+
+export default Vibe
