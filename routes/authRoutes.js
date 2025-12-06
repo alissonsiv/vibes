584eaa39a5408606a2003cda665525ea934622e9
alissonsiv <alissonmoura890@gmail.com>
2025-12-05 21:02:16 -0300

    Adição de sistema de flash messages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 // Rotas
 router.get('/login', AuthController.login);
 router.get('/register', AuthController.register);
+router.post('/register', AuthController.registerPost);
 
 export default router;
